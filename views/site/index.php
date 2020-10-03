08bc139bf18bfc10c3a78e064f14110ca335c095
shuinanxun <2424004764@qq.com>
2020-10-03 11:39:55 +0800

    网站首页说明文档更新

--- views/site/index.php
+++ views/site/index.php
@@ -9,3 +9,7 @@ $this->title = \Yii::$app->name;
 <p>他包含一个yii制作的api管理接口、一个vue + element-ui 开发的管理后台、一个uniapp开发的前端程序，一共三个部分</p>
 
 <p>目前开发进度正开始，时间2020-09-23 16：02</p>
+
+<p>api 文档使用docker 安装的yapi</p>
+
+<p>网关使用kong  http服务器是nginx，PHP版本7.4，mysql5.7，持续构建使用jenkins</p>
