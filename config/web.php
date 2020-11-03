0034d0fadc5f0563a1ab14df5a052f31acf3f41c
yifang <2424004764@qq.com>
2020-11-03 19:43:50 +0800

    采用pathinfo方式访问

--- config/web.php
+++ config/web.php
@@ -51,14 +51,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
