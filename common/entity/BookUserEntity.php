0cc4765f1ecc80a3e45e4bcf18e6ffd24a875451
yifang <2424004764@qq.com>
2020-11-22 11:28:35 +0800

    用户表增加性别字段

--- common/entity/BookUserEntity.php
+++ common/entity/BookUserEntity.php
@@ -10,6 +10,7 @@ use Yii;
  * @property int $user_id 用户表ID主键  保留1-100000  正常注册的id从100001开始  作为登陆账号
  * @property string $user_nikename 用户昵称
  * @property string $user_headimg 用户头像
+ * @property int $sex 性别 0 未知 1男 2女
  * @property int $status 状态 0正常 1禁用
  * @property string $birthday 用户生日  只填年月日 要区分阳历或阴历
  * @property int $birthday_type 日期类型 1阳历 2阴历
@@ -38,6 +39,7 @@ class BookUserEntity extends \app\common\BaseAR
             [['birthday', 'create_on'], 'safe'],
             [['user_nikename'], 'string', 'max' => 50],
             [['user_headimg', 'password_salt', 'password', 'bind_email'], 'string', 'max' => 255],
+            [['sex'], 'integer', 'min' => 0, 'max' => 2]
         ];
     }
 
@@ -50,6 +52,7 @@ class BookUserEntity extends \app\common\BaseAR
             'user_id' => '用户表ID主键  保留1-100000  正常注册的id从100001开始  作为登陆账号',
             'user_nikename' => '用户昵称',
             'user_headimg' => '用户头像',
+            'sex' => '性别 0 未知 1男 2女',
             'status' => '状态 0正常 1禁用',
             'birthday' => '用户生日  只填年月日 要区分阳历或阴历',
             'birthday_type' => '日期类型 1阳历 2阴历',
