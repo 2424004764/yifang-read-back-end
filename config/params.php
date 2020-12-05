fc493e34097e9bd84588a9abdfe5ae1bda26271d
yifang <2424004764@qq.com>
2020-12-05 20:44:05 +0800

    perf   配置文件中的smtp配置改为 保存在params.php中，但这个文件不构建到服务器

--- config/params.php
+++ config/params.php
@@ -5,4 +5,10 @@ return [
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
     'appName'   =>  '一方阅读',
+    'smtp'  =>  [
+        'host'  =>  'smtp.163.com',
+        'username' => 'yifangread@163.com',
+        'password' => 'JWEFJOETBREVTCNT',
+        'port' => '25',
+    ],
 ];
