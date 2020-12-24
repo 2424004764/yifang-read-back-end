a526d8f0a8de4ca81a1082bbb6ff0024b45b2e0c
yifang <2424004764@qq.com>
2020-12-24 13:26:01 +0800

    feat  进度表增加user_id字段，以及user_id、book_id、chapter_id 联合索引

--- common/entity/BookScheduleEntity.php
+++ common/entity/BookScheduleEntity.php
@@ -8,6 +8,7 @@ use Yii;
  * This is the model class for table "book_schedule".
  *
  * @property int $schedule_id 书籍章节进度表id主键
+ * @property int $user_id 用户id
  * @property int $book_id 书籍id
  * @property int $chapter_id 书籍的章节id
  * @property string $schedule 进度 某一个章节的进度百分比 还是 某个章节读到多少字了
@@ -29,7 +30,7 @@ class BookScheduleEntity extends \app\common\BaseAR
     public function rules()
     {
         return [
-            [['book_id', 'chapter_id'], 'integer'],
+            [['user_id', 'book_id', 'chapter_id'], 'integer'],
             [['create_on'], 'safe'],
             [['schedule'], 'string', 'max' => 10],
         ];
@@ -42,6 +43,7 @@ class BookScheduleEntity extends \app\common\BaseAR
     {
         return [
             'schedule_id' => '书籍章节进度表id主键',
+            'user_id'   =>  '用户id',
             'book_id' => '书籍id',
             'chapter_id' => '书籍的章节id',
             'schedule' => '进度 某一个章节的进度百分比 还是 某个章节读到多少字了',
